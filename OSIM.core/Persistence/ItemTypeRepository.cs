de0d1cbd1cc3b2cd83f533247fc36c9ab05a1697
Pelle <Pelle@LAPTOP-T69PBCOE>
2016-10-27 10:33:59 +0200

    Add project files.

---
+++ OSIM.core/Persistence/ItemTypeRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OSIM.Core.Entities;
+using NHibernate;
+
+namespace OSIM.Core.Persistence
+{
+    public interface IItemTypeRepository
+    {
+        int Save(ItemType itemType);
+    }
+
+    public class ItemTypeRepository : IItemTypeRepository
+    {
+
+        private ISessionFactory _sessionFactory;
+
+        public ItemTypeRepository(ISessionFactory sessionFactory)
+        {
+            _sessionFactory = sessionFactory;
+        }
+        public int Save(ItemType itemType)
+        {
+            int id;
+            using (var session = _sessionFactory.OpenSession())
+            {
+                id = (int)session.Save(itemType);
+                session.Flush();
+            }
+
+            return id;
+
+        }
+
+    }
+
+}
